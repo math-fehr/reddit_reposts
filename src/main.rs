55f56c8f9c8448dbd0af58fa8b095729dbd01dee
mfehr <mathieu.fehr@gmail.com>
2019-02-19 00:27:00 +0100

    Add tool to measure time.

--- src/main.rs
+++ src/main.rs
@@ -4,21 +4,51 @@ mod edit_state;
 mod possible_types;
 mod read_files;
 mod data_analysis;
+mod utils;
 
 use crate::reddit_post::RedditPost;
 use regex::Regex;
 use crate::read_files::*;
 use crate::data_analysis::*;
+use crate::utils::*;
 
 #[allow(dead_code)]
 fn get_url_regex() -> Regex {
     Regex::new(r"https?://(www.)?[-a-zA-Z0-9@:%._+~#=]{2,256}\.[a-z]{2,6}\b[-a-zA-Z0-9@:%_+.~#?&/=;]*").unwrap()
 }
 
+#[derive(Hash, Debug, PartialEq, Eq)]
+struct SimpleRedditPost {
+    created_utc: i32,
+    subreddit: String,
+}
+
+impl HasCreationDate for SimpleRedditPost {
+    fn get_creation_date(&self) -> i32 {
+        self.created_utc
+    }
+}
+
+impl HasSubreddit for SimpleRedditPost {
+    fn get_subreddit(&self) -> &str {
+        &self.subreddit
+    }
+}
+
+impl From<RedditPost> for SimpleRedditPost {
+    fn from(post: RedditPost) -> Self {
+        Self {
+            created_utc: post.created_utc,
+            subreddit: post.subreddit,
+        }
+    }
+}
+
 fn main() {
     let filepaths = vec!["datasets/RS_2011-02".to_string()];
     let item_iterator = JSONItemIterator::<RedditPost,_>::new(filepaths.clone().into_iter());
-    let map = get_links::<Box<RedditPost>, _>(item_iterator);
-    let map = get_reposts_accross_subreddits(map);
-    println!("{:#?}", map);
+    let (exec_ms, map) = measure_time(|| get_links::<SimpleRedditPost, _>(item_iterator));
+    println!("{:?}", exec_ms);
+    let (exec_ms, map) = measure_time(|| get_reposts_accross_subreddits(map));
+    println!("{:?}", exec_ms);
 }
---
+++ src/utils.rs
@@ -0,0 +1,10 @@
+use std::time::{Duration, Instant};
+
+/// Measure the execution time of the given closure
+#[allow(dead_code)]
+pub fn measure_time<T, F: FnOnce() -> T>(f: F) -> (Duration, T) {
+    let now = Instant::now();
+    let result = f();
+    (now.elapsed(), result)
+}
+
