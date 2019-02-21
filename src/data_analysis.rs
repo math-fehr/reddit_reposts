4793c9a9dba125ec99b4ac615956cd07eeee0a39
mfehr <mathieu.fehr@gmail.com>
2019-02-21 03:26:41 +0100

    Add function to get number of shared links between subreddits.
    Also, changed signature of get_links.

--- src/data_analysis.rs
+++ src/data_analysis.rs
@@ -6,13 +6,18 @@ pub use std::collections::{HashMap, HashSet};
 use std::hash::Hash;
 
 /// Get posts associated with the links
-pub fn get_links<T, IT>(iterator: IT) -> HashMap<String, HashSet<T>>
+pub fn get_links<T, IT>(iterator: IT, subreddits: Option<&HashSet<String>>) -> HashMap<String, HashSet<T>>
 where
     IT: Iterator<Item = RedditPost>,
     T: From<RedditPost> + Hash + Eq
 {
     let mut map = HashMap::new();
     for post in iterator {
+        if let Some(subreddits) = &subreddits {
+            if !subreddits.contains(&post.subreddit) {
+                continue;
+            }
+        }
         let url = post.get_linked_url();
         if let Some(url) = url {
             if !map.contains_key(&url) {
@@ -121,4 +126,34 @@ pub fn get_reposts_accross_subreddits<T>(links: HashMap<String, HashSet<T>>) -> 
         }
     }
     subreddits_reposts
+}
+
+/// Get the number of shared between subreddits.
+/// This count the number of time a link was posted in two different subreddits.
+pub fn get_shared_links_between_subreddits<T>(links: HashMap<String, HashSet<T>>) -> HashMap<String, HashMap<String, i32>> where
+                                         T: HasSubreddit + HasCreationDate + Eq + Hash {
+    let mut subreddits_links = HashMap::new();
+    for (_url, posts) in links.into_iter() {
+        if posts.len() <= 1 {
+            continue;
+        }
+        let subreddits: HashSet<_> = posts.iter().map(|p| p.get_subreddit()).collect();
+
+        for subreddit1 in subreddits.iter() {
+            if !subreddits_links.contains_key(*subreddit1) {
+                subreddits_links.insert(subreddit1.to_string(), HashMap::new());
+            }
+            let subreddit_links = subreddits_links.get_mut(*subreddit1).unwrap();
+            for subreddit2 in subreddits.iter() {
+                if subreddit1 == subreddit2 {
+                    continue;
+                }
+                if !subreddit_links.contains_key(*subreddit2) {
+                    subreddit_links.insert(subreddit2.to_string(), 0);
+                }
+                *subreddit_links.get_mut(*subreddit2).unwrap() += 1;
+            }
+        }
+    }
+    subreddits_links
 }
