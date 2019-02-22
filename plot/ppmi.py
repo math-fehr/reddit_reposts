db62b17b84fc895b5012fbfe34cf5c30f7a64c6a
mfehr <mathieu.fehr@gmail.com>
2019-02-22 08:42:01 +0100

    Add cli for python tsne file, and rename it.
    Also, add an image for the tsne.

--- plot/ppmi.py
+++ plot/ppmi.py
@@ -3,6 +3,7 @@ from sklearn.random_projection import sparse_random_matrix
 from scipy.sparse import csr_matrix
 import matplotlib.pyplot as plt
 from sklearn.manifold import TSNE
+import sys
 
 def read_ppmi(filepath):
     f = open(filepath, "r")
@@ -19,16 +20,15 @@ def read_ppmi(filepath):
         kernel.append(kernel_line)
     return (kernel, subreddits, is_nsfw)
 
-def main_ppmi():
-    (X, subreddits, is_nsfw) = read_ppmi("kernel")
-    X = TSNE(n_components=2, perplexity=5, learning_rate=10).fit_transform(X)
-    for i, l in enumerate(X):
-        if int(is_nsfw[i]) == 1:
-            color = 'red'
-        else:
-            color = 'blue'
-        plt.scatter(X[i,0], X[i,1], marker='x', color=color)
-        plt.text(X[i,0]+0.3, X[i,1]+0.3, subreddits[i], fontsize=9)
-    plt.show()
 
-main_ppmi()
+ppmi_filepath = sys.argv[1]
+(X, subreddits, is_nsfw) = read_ppmi(ppmi_filepath)
+X = TSNE(n_components=2, perplexity=5, learning_rate=5, random_state=42).fit_transform(X)
+for i, l in enumerate(X):
+    if int(is_nsfw[i]) == 1:
+        color = 'red'
+    else:
+        color = 'blue'
+    plt.scatter(X[i,0], X[i,1], marker='x', color=color)
+    plt.text(X[i,0]+0.3, X[i,1]+0.3, subreddits[i], fontsize=9)
+plt.show()
